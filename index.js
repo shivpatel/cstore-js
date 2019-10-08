0d65194241ad54ade1c4cfbf0f68b7617d5c5e7d
Shiv Patel <shivpatelssp@gmail.com>
2019-10-08 19:37:00 -0400

    cstore.download - download configs to random .env file on disk

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const parser = require('./lib/parser');
 const s3 = require('./lib/stores/s3');
 const ssm = require('./lib/stores/ssm');
 const secretsmanager = require('./lib/stores/secretsmanager');
+const fileSystem = require('./lib/file-system');
 
 const provide = {};
 
@@ -11,15 +12,13 @@ const stores = {
 }
 
 /**
- * Pull configs via cStore (S3) given the provided cstore.yml path and env tag.
+ * Get configs via cStore given the provided yml path and env tag. Returns as key/value object.
  * @param {String} tag Name of the desired env tag
- * @param {String} ymlPath Absolute path to cstore.yml file. Defaults to process.cwd()/cstore.yml
- * @param {Boolean} injectIntoProcess If `true`, env vars will be automatically
- * injected into process.env
+ * @param {String} ymlPath Absolute path to cstore.yml file
  * @param {Boolean} injectSecrets If `true`, env vars referencing secrets manager 
  * will be fetched and returned in decrypted form. 
  */
-provide.pull = async (tag, ymlPath = `${process.cwd()}/cstore.yml`, injectIntoProcess = true, injectSecrets = true) => {
+const getConfigs = async (tag, ymlPath, injectSecrets) => {
   console.info(`Loading configuration for ${tag}`);
   const doc = parser.parseYaml(ymlPath);
   const context = doc.context;
@@ -27,7 +26,7 @@ provide.pull = async (tag, ymlPath = `${process.cwd()}/cstore.yml`, injectIntoPr
   let envVarsMerged = {};
   for (let fileinfo of fileinfos) {
     if (!stores[fileinfo.store]) {
-      throw new Error(`cstore-pull - unsupported store type ${fileinfo.store}`);
+      throw new Error(`cstore-js - unsupported store type ${fileinfo.store}`);
     }
     const envVars = await stores[fileinfo.store](context, fileinfo);
     envVarsMerged = Object.assign(envVarsMerged, envVars);
@@ -36,6 +35,21 @@ provide.pull = async (tag, ymlPath = `${process.cwd()}/cstore.yml`, injectIntoPr
     await secretsmanager.findAllAndInject(context, envVarsMerged);
   }
   console.info(`Loaded configuration for ${tag}`);
+  return envVarsMerged;
+}
+
+/**
+ * Pull configs via cStore given the provided cstore.yml path and env tag. Returns as object.
+ * Will also inject straight into `process.env`.
+ * @param {String} tag Name of the desired env tag
+ * @param {String} ymlPath Absolute path to cstore.yml file. Defaults to process.cwd()/cstore.yml
+ * @param {Boolean} injectIntoProcess If `true`, env vars will be automatically
+ * injected into process.env
+ * @param {Boolean} injectSecrets If `true`, env vars referencing secrets manager 
+ * will be fetched and returned in decrypted form. 
+ */
+provide.pull = async (tag, ymlPath = `${process.cwd()}/cstore.yml`, injectIntoProcess = true, injectSecrets = true) => {
+  const envVarsMerged = await getConfigs(tag, ymlPath, injectSecrets);
   if (injectIntoProcess) {
     for (let envKey in envVarsMerged) {
       process.env[envKey] = envVarsMerged[envKey];
@@ -43,6 +57,25 @@ provide.pull = async (tag, ymlPath = `${process.cwd()}/cstore.yml`, injectIntoPr
     console.info('Injected configuration into process.env');
   }
   return envVarsMerged;
+}
+
+/**
+ * Pull configs via cStore given the provided cstore.yml path and env tag. Stores the results
+ * in a random.env file located at `process.cwd()`. Returns the absolute filepath for the random
+ * env file.
+ * @param {String} tag Name of the desired env tag
+ * @param {String} ymlPath Absolute path to cstore.yml file. Defaults to process.cwd()/cstore.yml
+ * @param {Boolean} injectSecrets If `true`, env vars referencing secrets manager 
+ * will be fetched and returned in decrypted form. 
+ */
+provide.download = async (tag, ymlPath = `${process.cwd()}/cstore.yml`, injectSecrets = true) => {
+  const envVarsMerged = await getConfigs(tag, ymlPath, injectSecrets);
+  let dataToWrite = '';
+  for (let envKey in envVarsMerged) {
+    dataToWrite += `${envKey}=${envVarsMerged[envKey]}\n`;
+  }
+  const filepath = await fileSystem.saveToRandomLocation(dataToWrite);
+  return filepath;
 }
 
 module.exports = provide;
